f9d8a99509d7ecf6645a2dc3fa364d0de660cf9a
Sam May <sammaxwellmay@gmail.com>
2025-06-25 13:26:43 +0100

    Feedback Round 4 - Lead Gen FontAwesome

--- app/public/wp-content/themes/aimpro/page-lead-generation.php
+++ app/public/wp-content/themes/aimpro/page-lead-generation.php
@@ -125,22 +125,10 @@ get_header(); ?>
                     }
 
                     $method_icons = array(
-                        '<svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M21 21L16.514 16.506M19 10.5C19 15.194 15.194 19 10.5 19C5.806 19 2 15.194 2 10.5C2 5.806 5.806 2 10.5 2C15.194 2 19 5.806 19 10.5Z" stroke="currentcolour" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>',
-                        '<svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M18 20V10" stroke="currentcolour" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M12 20V4" stroke="currentcolour" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                            <path d="M6 20V14" stroke="currentcolour" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>',
-                        '<svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M14 2H6C5.46957 2 4.96086 2.21071 4.58579 2.58579C4.21071 2.96086 4 3.46957 4 4V20C4 20.5304 4.21071 21.0391 4.58579 21.4142C4.96086 21.7893 5.46957 22 6 22H18C18.5304 22 19.0391 21.7893 19.4142 21.4142C19.7893 21.0391 20 20.5304 20 20V8L14 2Z" stroke="currentcolour" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                            <polyline points="14,2 14,8 20,8" stroke="currentcolour" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>',
-                        '<svg width="48" height="48" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M4 4H20C21.1 4 22 4.9 22 6V18C22 19.1 21.1 20 20 20H4C2.9 20 2 19.1 2 18V6C2 4.9 2.9 4 4 4Z" stroke="currentcolour" stroke-width="2"/>
-                            <polyline points="22,6 12,13 2,6" stroke="currentcolour" stroke-width="2"/>
-                        </svg>'
+                        '<i class="fas fa-search" style="font-size: 48px;"></i>', // Search Engine Marketing
+                        '<i class="fab fa-facebook" style="font-size: 48px;"></i>', // Social Media Lead Generation
+                        '<i class="fas fa-file-alt" style="font-size: 48px;"></i>', // Content Marketing & Lead Magnets
+                        '<i class="fas fa-envelope" style="font-size: 48px;"></i>' // Email Marketing & Nurturing
                     );
 
                     foreach ($methods as $index => $method) :
